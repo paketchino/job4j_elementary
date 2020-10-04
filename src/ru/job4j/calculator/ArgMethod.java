b20aa230d0cee2443c99e1b8af4c7dbbef07a7c5
paketchino <chansgorn@gmail.com>
2020-10-04 21:49:26 +0300

    3.1. Загрузка файлов в github. [#243562]

---
+++ src/ru/job4j/calculator/ArgMethod.java
@@ -0,0 +1,13 @@
+package ru.job4j.calculator;
+
+public class ArgMethod {
+
+    public static void hello(String name) {
+        System.out.println("Hello, " + name);
+    }
+
+    public static void main(String[] args) {
+        String name = "Roman Kulyanin";
+        ArgMethod.hello(name);
+    }
+}
