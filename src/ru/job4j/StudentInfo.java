714410711ae89b7dc61e63091b55af751eb4f521
paketchino <chansgorn@gmail.com>
2020-10-04 20:30:51 +0300

    3.1. Загрузка файлов в github. [#243562]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Roman Kulyanin");
+        System.out.println("29.09.2000");
+    }
+}
---
+++ src/ru/job4j/calculator/Calculator.java
@@ -0,0 +1,18 @@
+package ru.job4j.calculator;
+
+public class Calculator {
+    public static void main(String[] args) {
+        int one = 1;
+        int two = 2;
+        int onePlusTwo = one + two;
+        int six = 6;
+        int sixDivTwo = six / two;
+        System.out.println(sixDivTwo);
+        int four = 4;
+        int fourTimeTwo = four * two;
+        System.out.println(fourTimeTwo);
+        int five = 5;
+        int fiveMinusTwo = five - two;
+        System.out.println(fiveMinusTwo);
+    }
+}
