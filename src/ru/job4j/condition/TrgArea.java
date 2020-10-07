57c208c62979831ea3745ecb6fda9c10a0e67f99
paketchino <chansgorn@gmail.com>
2020-10-07 18:02:05 +0300

    3.1. Загрузка файлов в github. [#243562]

---
+++ src/ru/job4j/condition/TrgArea.java
@@ -0,0 +1,17 @@
+package ru.job4j.condition;
+
+public class TrgArea {
+    public static double area(double a, double b, double c) {
+        double p = (a + b + c) /2;
+        double first = (p-a);
+        double second = (p-b);
+        double three = (p-c);
+        double four = p * (first + second + three);
+        double five = Math.sqrt(four);
+        return five;
+    }
+    public static void main(String[] args) {
+        double rsl = TrgArea.area(2,2,2);
+        System.out.println("are(2,2,2) = " + rsl);
+    }
+}
