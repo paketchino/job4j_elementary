6369a2bb241253e83a10c4f469555deeec118379
paketchino <chansgorn@gmail.com>
2020-10-07 13:13:31 +0300

    3.1. Загрузка файлов в github. [#243562]

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -2,15 +2,13 @@ package ru.job4j.condition;
 
 public class Point {
     public static double distance(int x1, int y1, int x2, int y2) {
-        double rsl = (x2 - x1) + (y2 - y1);
-        Math.pow(x2,x1);
-        Math.pow(y2,y1);
-        Math.sqrt(rsl);
+        double rsl = Math.pow((x2-x1),2)+ Math.pow((y2-y1),2);
         return rsl;
     }
     public static void main(String[] args) {
-        double distance = Point.distance(0,0,2,0);
-        double result = Point.distance(0,0,2,0);
-        System.out.println("result(0,0) to (2,0)" + result);
+        double result1 = Point.distance(0,0,2,0);
+        System.out.println("result(0,0) to (2,0)" + result1);
+        double result2 =  Math.sqrt(distance(0,0,2,0));
+        System.out.println(result2);
     }
-}
+    }
