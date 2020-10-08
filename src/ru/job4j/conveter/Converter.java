572232d758263e3b2ff59e8124aec98c6eb90548
paketchino <chansgorn@gmail.com>
2020-10-09 00:01:34 +0300

    3.1. Загрузка файлов в github. [#243562]

--- src/ru/job4j/conveter/Converter.java
+++ src/ru/job4j/conveter/Converter.java
@@ -3,17 +3,30 @@ package ru.job4j.conveter;
 public class Converter {
 
     public static  int rubleToEuro(int value) {
-        return value / 60;
-    }
-
-    public static int rubleToDollar(int value) {
         return value / 70;
     }
 
+    public static int rubleToDollar(int value) {
+        return value / 60;
+    }
+
     public static void main(String[] args) {
-        int euro = Converter.rubleToEuro(140);
+        int euro = Converter.rubleToEuro(280);
         int dollar = Converter.rubleToDollar(120);
         System.out.println("140 rubles are " + euro + " euro.");
         System.out.println("120 rubles are " + dollar + " dollar.");
+
+        int inEuro = 140;
+        int expectedEuro = 2;
+        int outEuro = Converter.rubleToEuro(inEuro);
+        boolean passedEuro = expectedEuro == outEuro;
+        System.out.println("140 rubles are. Test result : " +  passedEuro);
+
+        int inDollar = 120;
+        int expectedDollar = 2;
+        int outDollar = Converter.rubleToDollar(inDollar);
+        boolean passedDollar = expectedDollar == outDollar;
+        System.out.println("120 rubles are. Test result : " + passedDollar);
     }
 }
+
