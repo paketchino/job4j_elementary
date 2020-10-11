f9ab4340d38cca1366460040feda0481cc90e9a9
paketchino <chansgorn@gmail.com>
2020-10-11 20:57:46 +0300

    Cinema.java

--- src/ru/job4j/condition/Cinema.java
+++ src/ru/job4j/condition/Cinema.java
@@ -3,7 +3,7 @@ package ru.job4j.condition;
 public class Cinema {
     public static void access(int age) {
         System.out.println("The age of the customer is " + age);
-        if (age >== 21) {
+        if (age >= 21) {
 
             System.out.println("Welocome to the cinema is : ");
         } else  {
