3cf68faeb9b1340249e277be970bbf807b87ae87
paketchino <chansgorn@gmail.com>
2020-10-14 19:15:08 +0300

    Cinema.java

--- src/ru/job4j/condition/Cinema.java
+++ src/ru/job4j/condition/Cinema.java
@@ -1,19 +1,19 @@
 package ru.job4j.condition;
 
 public class Cinema {
-    public static void access(int age) {
-        System.out.println("The age of the customer is " + age);
-        if (age >= 21) {
-
-            System.out.println("Welocome to the cinema is : ");
-        } else  {
-            System.out.println("It is not for you");
+    public static void permission(boolean allowParent, boolean hasMoney) {
+        if (allowParent && hasMoney) {
+            System.out.println("I can go to the cinema");
+        } else {
+            System.out.println("I can`t.");
         }
-    }
+        }
 
-    public static  void main(String[] args) {
-        Cinema.access(21);
-        Cinema.access(16);
+    public static void main(String[] args) {
+        permission(true, true);
+        permission(true, false);
+        permission(false, true);
+        permission(false, false);
     }
-}
+    }
 
