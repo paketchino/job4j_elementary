97f94ad7fc260715c5d0a9f21124c557f7c5bbb9
paketchino <chansgorn@gmail.com>
2020-10-16 21:50:03 +0300

    MultiMax.java

---
+++ src/ru/job4j/condition/MultiMax.java
@@ -0,0 +1,15 @@
+package ru.job4j.condition;
+
+public class MultiMax {
+    public static int max(int first, int second, int third) {
+        int result = first > second ? first : second;
+        return result > third ? result : second;
+    }
+
+    public static void main(String[] args) {
+        int result = max(1, 4, 2);
+        System.out.println(result);
+        int result1 = max(3, 3, 3);
+        System.out.println(result1);
+    }
+}
---
+++ test/ru/job4j/condition/MultiMaxTest.java
@@ -0,0 +1,21 @@
+package ru.job4j.condition;
+
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import static org.junit.Assert.*;
+
+public class MultiMaxTest {
+    @Test
+    public void whenSecondMax() {
+        int result = MultiMax.max(1, 4, 2);
+        assertThat(result, is(4));
+    }
+    @Test
+    public void whenFirstSecondThirdMax() {
+        int result = MultiMax.max(3, 3, 3);
+        assertThat(result, is(3));
+    }
+}
