61432db1b8cf655c10967e53c298b18537ada802
paketchino <chansgorn@gmail.com>
2020-10-17 16:55:26 +0300

    MultiMax.java

--- src/ru/job4j/condition/MultiMax.java
+++ src/ru/job4j/condition/MultiMax.java
@@ -3,7 +3,7 @@ package ru.job4j.condition;
 public class MultiMax {
     public static int max(int first, int second, int third) {
         int result = first > second ? first : second;
-        return result > third ? result : second;
+        return result > third ? result : third;
     }
 
     public static void main(String[] args) {
