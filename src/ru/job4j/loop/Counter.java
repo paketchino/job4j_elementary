4387729ee9d29c290ea9722ad6dcd3c4f129ee65
paketchino <chansgorn@gmail.com>
2020-10-19 23:01:06 +0300

    Counter.java

--- src/ru/job4j/loop/Counter.java
+++ src/ru/job4j/loop/Counter.java
@@ -8,9 +8,25 @@ public class Counter {
         }
         return sum;
     }
+
+    public static int sumByEven(int start, int finish) {
+        int sum = 0;
+        for (int num = start; num <= finish; num++) {
+            if (num % 2 == 0 ) {
+                sum = sum + num;
+            }
+        }
+        return sum;
+    }
+
+
     public static void main(String[] args) {
         System.out.println(sum(0, 5));
         System.out.println(sum(3, 8));
         System.out.println(sum(1, 1));
+
+        System.out.println(sumByEven(0, 10));
+        System.out.println(sumByEven(3, 8));
+        System.out.println(sumByEven(1, 1));
     }
 }
