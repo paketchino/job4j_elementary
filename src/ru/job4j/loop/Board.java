2bd8087d67bf7cf2ecc49f347ca4a09357204715
paketchino <chansgorn@gmail.com>
2020-10-21 21:16:54 +0300

    Построение шахматной доски

--- src/ru/job4j/loop/Board.java
+++ src/ru/job4j/loop/Board.java
@@ -19,6 +19,6 @@ public class Board {
         public static void main(String[] args) {
             paint(3, 3);
             System.out.println();
-            paint(5, 4);
+            paint(5 , 4);
         }
     }
