9ac5aa0ad276250a0b39e9ca96f9362f840c7b9b
paketchino <chansgorn@gmail.com>
2020-10-21 21:15:09 +0300

    Построение шахматной доски в псевдографике

---
+++ src/ru/job4j/loop/Board.java
@@ -0,0 +1,24 @@
+package ru.job4j.loop;
+
+import jdk.swing.interop.SwingInterOpUtils;
+
+public class Board {
+    public static void paint(int width, int height) {
+        for (int row = 0; row < height; row++) {   // создали ряд который не превышает высоты  доски
+            for (int cell = 0; cell < width; cell++) { // аналогично создали ячейку которая не превышает ширину доски
+            if ((cell + row) % 2 == 0) {
+                System.out.print("X");
+            } else {
+                System.out.print(" ");
+            }
+            }
+            System.out.println();
+        }
+    }
+
+        public static void main(String[] args) {
+            paint(3, 3);
+            System.out.println();
+            paint(5, 4);
+        }
+    }
