2e6936f9091e366e5450dfb9b55dc26c8840392b
paketchino <chansgorn@gmail.com>
2020-10-23 22:21:51 +0300

    Проверка простого числа

--- src/ru/job4j/loop/CheckPrimeNumber.java
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -3,17 +3,20 @@ package ru.job4j.loop;
 public class CheckPrimeNumber {
     public static boolean check(int number) {
         boolean prime = true;
+        if (number == 1) {
+            prime = false;
+        }
         for (int i = 2; i < number; i++) {
             if ((number % i) == 0) {
                 prime = false;
                 break;
-
             }
         }
         return prime;
     }
+
     public static void main(String[] args) {
-        check(7);
+        check(1);
         check(4);
     }
 }
--- test/ru/job4j/loop/CheckPrimeNumberTest.java
+++ test/ru/job4j/loop/CheckPrimeNumberTest.java
@@ -9,14 +9,20 @@ import static org.junit.Assert.*;
 public class CheckPrimeNumberTest {
 
     @Test
-    public void whenPrimeNumberEqual5() {
+    public void whenPrimeNumberEqual7() {
         boolean check = CheckPrimeNumber.check(7);
-      assertThat(check, is(true));
+        assertThat(check, is(true));
     }
 
     @Test
     public void whenPrimeNumberEqual8() {
         boolean check = CheckPrimeNumber.check(8);
+        assertThat(check, is(false));
+    }
+
+    @Test
+    public void whenPrimeNumberEqual1() {
+        boolean check = CheckPrimeNumber.check(1);
         assertThat(check, is(false));
     }
 }
