c9cb6f552fa6b793bb5dc039dd61ede0170fe619
paketchino <chansgorn@gmail.com>
2020-10-29 23:22:20 +0300

    ArrayDefinition.java

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -6,18 +6,9 @@ public class ArrayDefinition {
         String[] surnames = new String[100500];
         float[] prices = new float[100];
         int[] ints = new int[10];
-        String[] names = new String[4];
-        names[0] = "Petr Arsentiev";
-        names[1] = "Korobeinikov Stas";
-        names[2] = "Roman Kulyanin";
-        names[3] = "Viktor";
-        System.out.println(names[0]);
-        System.out.println(names[1]);
-        System.out.println(names[2]);
-        System.out.println(names[3]);
-        System.out.println("Размер массива равен: " + ints.length);
-        System.out.println("Размер массива равен: " + prices.length);
-        System.out.println("Размер массива равен: " + surnames.length);
-        System.out.println("Размер массива равен: " + ages.length);
+        String[] names = {"Petr Arsentiev", "Korobeinikov Stas", "Roman Kulyanin", "Viktor"};
+        for (String name : names) {
+            System.out.println(name);
+        }
     }
 }
