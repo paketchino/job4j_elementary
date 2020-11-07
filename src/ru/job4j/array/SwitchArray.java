644d7f32ca96bdd9604099b06a3d4d0fd9b173ac
paketchino <chansgorn@gmail.com>
2020-11-07 15:09:07 +0300

    Устранение ошибок

--- src/ru/job4j/array/SwitchArray.java
+++ src/ru/job4j/array/SwitchArray.java
@@ -1,10 +1,17 @@
 package ru.job4j.array;
 
 public class SwitchArray {
+    public static int[] swap(int[] array, int source, int dest) {
+       int temp = array[dest]; //создали переменную для временного хранилища
+       array[dest] = array[source]; //записываем в ячейку с индексом source значение другой ячейки dest
+       array[source] = temp; //записываем в ячейку с индексом source временную переменной dest
+        return array;
+    }
+
     public static int[] swapBorder(int[] array) {
         int temp = array[0];
-        array[0] = array[5];
-        array[5] = temp;
+        array[0] = array[array.length - 1];
+        array[array.length - 1] = temp;
         return array;
     }
 
