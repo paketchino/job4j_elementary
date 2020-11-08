2284e27e6347838d6f1a8a278d9e50aeca9a2872
paketchino <chansgorn@gmail.com>
2020-11-08 11:25:44 +0300

    Поиск индекса в диапозоне

--- test/ru/job4j/array/FindLoopTest.java
+++ test/ru/job4j/array/FindLoopTest.java
@@ -8,10 +8,21 @@ public class FindLoopTest {
 //32
     @Test
     public void whenArrayHas5Then0() {
-        int input[] = {5, 4, 3, 2};
-        int value = 5;
+        int[] input = {5, 4, 3, 2};
+        int value = 3;
         int result = FindLoop.indexOf(input, value);
-        int expect = 0;
+        int expect = 2;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void find3() {
+        int[] input = new int[] {5, 2, 10, 2, 4};
+        int value = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = 3;
         assertThat(result, is(expect));
     }
 }
